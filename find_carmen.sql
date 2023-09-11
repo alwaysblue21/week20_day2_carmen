d2c5092839fd82eb515780d7196dc14b345b0f3a
Je Yun <alwaysblue21@gmail.com>
2023-09-11 18:25:54 -0400

    carmen...

--- find_carmen.sql
+++ find_carmen.sql
@@ -71,8 +71,9 @@ WHERE code ILIKE 'BRA';
 
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
+-- 91084
+SELECT * FROM city
+WHERE population = 91084;
 
 
-
-
--- She's in ____________________________!
+-- She's in _california _!
